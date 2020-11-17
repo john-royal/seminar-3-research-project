ebf548b7b0d9c51cfe73630fcadaaa9c09852a3e
John Royal <34844819+john-royal@users.noreply.github.com>
2020-11-17 14:42:54 -0500

    Update static file routes

--- server/plugins.js
+++ server/plugins.js
@@ -19,14 +19,14 @@ module.exports = fp(async function (fastify, opts) {
   // Serve static assets
   fastify.register(FastifyStatic, {
     root: path.join(__dirname, '../public'),
-    prefix: '/assets',
+    prefix: '/static',
     decorateReply: false
   })
 
   // Serve built assets
   fastify.register(FastifyStatic, {
     root: path.join(__dirname, '../dist'),
-    prefix: '/assets/js',
+    prefix: '/static/js',
     decorateReply: false
   })
 })
