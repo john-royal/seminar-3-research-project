42b5313dc858cce127e32a7756cf16e40635814c
John Royal <34844819+john-royal@users.noreply.github.com>
2020-11-17 14:55:54 -0500

    Update study photos route to use data from models

--- server/routes/study-photos.js
+++ server/routes/study-photos.js
@@ -1,34 +1,12 @@
 'use strict'
 
-const fs = require('fs').promises
-const path = require('path')
-
-const internals = {
-  IMAGE_DIRECTORY: path.join(__dirname, '../../public/img'),
-  getRandom (arr, n) {
-    const result = new Array(n)
-    let len = arr.length
-    const taken = new Array(len)
-    if (n > len) { throw new RangeError('getRandom: more elements taken than available') }
-    while (n--) {
-      const x = Math.floor(Math.random() * len)
-      result[n] = arr[x in taken ? taken[x] : x]
-      taken[x] = --len in taken ? taken[len] : len
-    }
-    return result
-  }
-}
+const { Photo } = require('../models/content')
 
 module.exports = async function (fastify, options) {
-  const images = await fs.readdir(internals.IMAGE_DIRECTORY)
-  const getRandomImages = () => {
-    return internals.getRandom(images, 16).map(image => `/assets/img/${image}`)
-  }
-
   fastify.get('/steps/1', async function (request, reply) {
     return reply.view('study-photos', {
       trial: 'Trial 1 of 3',
-      images: getRandomImages(),
+      images: Photo.getRandom(16),
       timer: {
         label: 'Photos will be hidden in',
         length: 1 / 6,
