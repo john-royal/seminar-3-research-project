331e31b0ec8ad4e6258041fa255c5ec97b45fc22
John Royal <34844819+john-royal@users.noreply.github.com>
2020-11-18 09:08:18 -0500

    Refactor views and routes

--- server/routes/start.js
+++
@@ -1,17 +0,0 @@
-'use strict'
-
-module.exports = async function (fastify, options) {
-  fastify.get('/', async function (request, reply) {
-    return reply.view('splash', {
-      heading: 'Effects of Music Genre on Memory and Recollection',
-      subheading: [
-        'For this study, you will complete a few trials of a task meant to test your memory and recollection.',
-        'This will take about 15 minutes.'
-      ],
-      button: {
-        label: 'Start',
-        destination: '/register'
-      }
-    })
-  })
-}
